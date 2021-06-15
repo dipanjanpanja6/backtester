034043be7dec28f652c75484084537ae56f487e8
Dipanjan Panja <dipanjan.panja@geogo.in>
2021-06-16 03:42:21 +0530

    feat(add navigation/menu): ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,17 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import { Main } from './screens/Main';
 import { Login } from './screens/Login';
+import { Grid } from '@material-ui/core';
+import TabPanel from '../src/components/TabPanel';
+import Appbar from '../src/components/AppBar';
 
 function App() {
   const [user, setUser] = React.useState<null | firebase.User>(null);
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue)
+  }
 
   React.useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -16,7 +24,17 @@ function App() {
   });
 
   if (user) {
-    return <Main />;
+    return (
+      <Grid container>
+        <Appbar {...{ handleChange, value }} />
+        <TabPanel value={value} index={0}>
+          <Main />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          empty item
+        </TabPanel>
+      </Grid>
+    )
   }
 
   return <Login />;
