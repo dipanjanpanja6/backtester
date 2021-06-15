5aae1d68e586474943964012d5bda94480d2cc02
Walter <codesigngo@gmail.com>
2021-06-15 19:17:17 +0700

    revert(Revert "config to use  emulator for authentication"): firebase
    This reverts commit 638066cafa02bd2f5af33fbb5ee71f77e4117a12.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,16 +1,7 @@
 import firebase from "firebase/app";
-import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGzv_ewiHYwFPhFoICNuZpNzB7l8PvSDs",
-  authDomain: "evergreen-trading-system.firebaseapp.com",
-  projectId: "evergreen-trading-system",
-  storageBucket: "evergreen-trading-system.appspot.com",
-  messagingSenderId: "211985681454",
-  appId: "1:211985681454:web:215134dae6e58b644ee3bf",
-  measurementId: "G-4KNWLB3H0F"
+  // please create a web app on Firebase Console to get the configuration (https://console.firebase.google.com)
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-firebase.auth().useEmulator("http://localhost:9099")
