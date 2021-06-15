bdec0aeb1c66f36774a2db55cdf72aa96ec7d3f2
Walter <codesigngo@gmail.com>
2021-06-15 18:06:20 +0700

    feat(config to use  emulator for authentication): firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,16 @@
 import firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
-  // please create a web app on Firebase Console to get the configuration (https://console.firebase.google.com)
+  apiKey: "AIzaSyAGzv_ewiHYwFPhFoICNuZpNzB7l8PvSDs",
+  authDomain: "evergreen-trading-system.firebaseapp.com",
+  projectId: "evergreen-trading-system",
+  storageBucket: "evergreen-trading-system.appspot.com",
+  messagingSenderId: "211985681454",
+  appId: "1:211985681454:web:215134dae6e58b644ee3bf",
+  measurementId: "G-4KNWLB3H0F"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+firebase.auth().useEmulator("http://localhost:9099")
